0768ad0a3c92db9d5467382af832224659b3ca9e
Luis <sc.luis@gmail.com>
2018-10-21 14:08:10 +0100

    Refactoring components and routes

---
+++ AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        Add a Help page.
+    </div>
+);
+
+export default AddExpensePage;
---
+++ EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        Add a edit page.
+    </div>
+);
+
+export default EditExpensePage;
