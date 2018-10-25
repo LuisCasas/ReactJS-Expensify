4e40ed5ad7aeaab74e2210779d4dd0ae6ed13b8a
LuisCasas <sc.luis@gmail.com>
2018-10-25 21:19:20 +0100

    ES6 Deconstruring arrays syntax

--- src/playground/deconstruring.js
+++ src/playground/deconstruring.js
@@ -10,6 +10,7 @@ const person = {
     }
 };
 
+
 // const name = person.name;
 // const age = person.age;
 
@@ -19,7 +20,6 @@ console.log(`${firstName} is ${age}`);
 const {city, country: countryName} = location;
 
 console.log(`${city} is in ${countryName}`);
-*/
 
 const album = {
     title: 'Master of Puppets',
@@ -35,3 +35,19 @@ const {title, band, publisher} = album;
 const {name: publisherName = 'self published'} = publisher;
 
 console.log(`${title} from ${band} was published by ${publisherName}`);
+
+*/
+
+
+const address = ['32 London Road', 'Edinburgh', 'Scotland', 'EH5'];
+const [street, city, country, postcode = 'EH'] = address;
+
+// console.log(`Address is ${address[0]}, city: ${address[1]}`);
+console.log(`Address is ${street}, city: ${country}`);
+
+
+const item = ['Coffee Late', '2.00', '2.50', '3.00'];
+const [drink, small, medium, larage] = item;
+
+console.log(`The price of a ${drink} medium size is ${medium}`);
+
