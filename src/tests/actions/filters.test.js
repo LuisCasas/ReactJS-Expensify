f36094958a203c823123dfdd275a4f17349d5cf9
LuisCasas <sc.luis@gmail.com>
2018-11-27 22:29:23 +0000

    Adding test for actions filters

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,71 @@
+import moment from 'moment';
+import { setStartDate, setEndDate, sortByAmount, sortByDate, setTextFilter } from '../../actions/filters';
+
+test('Should generate set start date', () => {
+    const action = setStartDate(moment(0));
+
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    });
+});
+
+test('Should generate set end date', () => {
+    const action = setEndDate(moment(0));
+
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    });
+});
+
+test('Should sort by amount', () => {
+    const action = sortByAmount();
+
+    expect(action).toEqual({
+        type: 'SORT_BY_AMOUNT'
+    });
+});
+
+test('Should sort by date', () => {
+    const action = sortByDate();
+
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE'
+    });
+});
+
+
+test('Should set Text filter with string', () => {
+    const action = setTextFilter('rent');
+
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'rent'
+    });
+});
+
+test('Should set Text filter with default empty value', () => {
+    const action = setTextFilter();
+
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    });
+});
+/*
+
+export const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+export const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
+export const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+*/
