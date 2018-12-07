d548ee58a01ffa9e08f4b04628379bb32aabd1ab
LuisCasas <sc.luis@gmail.com>
2018-12-07 20:55:15 +0000

    Setting up snpashot tests

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,18 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Header from '../../components/Header';
 
 
 test('Should render Header', () => {
-    const renderer = new ReactShallowRenderer();
 
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
 
- //   console.log(renderer.getRenderOutput());
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    // expect(wrapper.find('h1').length).toBe(1);
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
