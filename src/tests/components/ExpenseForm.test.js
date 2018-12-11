b7f11b4250721752c25aac175302beae68900d39
LuisCasas <sc.luis@gmail.com>
2018-12-11 23:28:43 +0000

    Test Date picker

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
+import moment from 'moment';
 
 test('Should render ExpenseForm component', () => {
     const wrapper = shallow(<ExpenseForm />);
@@ -64,4 +65,32 @@ test('Should set an invalid amount value', () => {
     });
 
     expect(wrapper.state('amount')).toBe('');
+});
+
+test('Should call onsubmit prop for valid submittion', () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[2]} onSubmit={onSubmitSpy} />);
+
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    
+    expect(wrapper.state('error')).toBe('');
+
+    expect(onSubmitSpy).toHaveBeenCalledWith({
+        description: expenses[2].description,
+        amount: expenses[2].amount,
+        note: expenses[2].note,
+        createdAt: expenses[2].createdAt
+    });
+});
+
+
+test('Should set new date', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+
+    expect(wrapper.state('createdAt')).toEqual(now);
+
 });
