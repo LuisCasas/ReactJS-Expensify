ed1d3836a009f11dfc723b2777c7f5d347ce2f2b
LuisCasas <sc.luis@gmail.com>
2019-01-13 16:23:59 +0000

    Update Add/Edit expense button text based on new or edit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -98,7 +98,7 @@ export default class ExpenseForm extends React.Component{
                         onChange={this.onNoteChange}
                     >
                     </textarea>
-                    <button>Add Expense</button>
+                    <button>{this.state.description === '' ? 'Add' : 'Edit'} Expense</button>
                 </form>
 
             </div>
