ddc54bfba33866f242dae53db95a6b37aca0c5e2
LuisCasas <sc.luis@gmail.com>
2019-01-15 21:25:55 +0000

    Adding redirect Login/Logout

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { startSetExpenses } from './actions/expenses';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configure';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
@@ -10,23 +10,32 @@ import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
 const jsx = (
     <Provider store={store} >
         <AppRouter />
     </Provider>
 );
 
-ReactDOM.render(<p>Loading data...</p>, document.getElementById('app'));
+let hasRendered = false; 
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading data...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user){
-        console.log('Logged In');
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if(history.location.pathname === '/'){
+                history.push('/dashboard');
+            }
+        });        
     } else {
-        console.log('Logged Out');
+        renderApp();
+        history.push('/');
     }
 });
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Router, Route, Switch} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import EditExpensePage from '../components/EditExpensePage';
@@ -8,8 +9,10 @@ import Header from '../components/Header';
 import NotFound from '../components/NotFound';
 import LoginPage from '../components/LoginPage';
 
+export const history = createHistory();
+
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <div>
             <Header />
             <Switch>
@@ -22,7 +25,7 @@ const AppRouter = () => (
                 <Route component={NotFound}  />
             </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter;
