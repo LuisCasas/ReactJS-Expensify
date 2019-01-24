736218c96f22069d328d2a1bfe1be23f3393c7a1
LuisCasas <sc.luis@gmail.com>
2019-01-24 21:11:36 +0000

    Resolve Babel-polyfill error

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = (env) => {
 
     // console.log(env);
     return {
-        entry: ['babel-polifill', './src/app.js'],
+        entry: ['babel-polyfill', './src/app.js'],
         output: {
             path: public_dir,
             filename: 'bundle.js'
